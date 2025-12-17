4ce2edb1722ef6efd5c0fee37727fa066972da25
Norayr Chilingarian <norayr@arnet.am>
2025-12-18 01:59:32 +0400

    bitrate

--- run.sh
+++ run.sh
@@ -1,5 +1,6 @@
 #./spartan-waves -format mp3 -host radio.norayr.am -bitrate-kbps 320 -playlist playlist.txt -shuffle
 #./spartan-waves -format ogg -playlist playlist_ogg.txt -bitrate-kbps 499 -host radio.norayr.am -shuffle
 #./spartan-waves -source wav -music-dir ./music -shuffle -playlist playlist.txt -host radio.norayr.am
-./spartan-waves -playlist playlist.txt -shuffle -host radio.norayr.am -stream-name "inky from the tape sets"
+#./spartan-waves -playlist playlist.txt -shuffle -host radio.norayr.am -stream-name "inky from the tape sets"
+./spartan-waves -playlist playlist.txt -shuffle -bitrate-kbps 256 -host radio.norayr.am -stream-name "inky from the tape sets"
 
