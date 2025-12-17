9e525d22ef2af0cafa8fe9aca7c5a7e4cdb0230b
Norayr Chilingarian <norayr@arnet.am>
2025-12-17 18:52:30 +0400

    shuffle

--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
 #./spartan-waves -format mp3 -host radio.norayr.am -bitrate-kbps 320 -playlist playlist.txt -shuffle
-./spartan-waves -format ogg -playlist playlist_ogg.txt -bitrate-kbps 499 -host radio.norayr.am
+./spartan-waves -format ogg -playlist playlist_ogg.txt -bitrate-kbps 499 -host radio.norayr.am -shuffle
 
